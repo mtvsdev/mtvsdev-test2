e29c6e1a520bfd1fd3e7a1808c0c1162c4fdcdda
lumn93 <lumn93@gmail.com>
2023-05-31 17:35:20 +0900

    5:35pm jinwoo 참가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,10 @@
 public class Application {
     public static void main(String[] args){
         System.out.println("hello");
+        //jinwoo 참가
+
+        System.out.println("world");
+
+
     }
 }
